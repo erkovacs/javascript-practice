b3fcb7cfce2553c7ddb1c25a9d181db1944ede4c
Kovacs <ekovacs@techteam.com>
2018-09-18 11:48:58 +0300

    added better debugging

--- debug/print_r.js
+++ debug/print_r.js
@@ -33,3 +33,22 @@ var print_r = function(that)
     
     return retval || "{}";
 }
+
+// Better
+var parseObject = function(obj){
+    var out = '';
+    if(typeof obj === 'object'){
+        for(var prop in obj){
+            if(obj[prop] !== obj && obj.hasOwnProperty(prop) && typeof obj[prop] !== 'function'){
+                if(typeof obj[prop] === 'object'){
+                    out += parseObject(obj[prop]);
+                } else {
+                    out += "\n\r["+prop+"] => "+obj[prop]+";\n\r";
+                }
+            }
+        }
+    } else {
+        out += obj;
+    }
+    return out;
+};
